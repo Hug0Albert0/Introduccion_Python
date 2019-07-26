444c4a108672c8e97b93ed8ccfcd7991b701b4e9
hug0albert0 <hugorivera.cool.player@gmail.com>
2019-07-26 14:59:12 -0500

    Introduccion & variables

---
+++ introduccion.py
@@ -0,0 +1,18 @@
+print("Hola Mundo")
+print ("Bienvenido a NodeSchool")
+print ("Mi nombre es Fulanito")
+print ("Tengo 20 años")
+print ("y me gusta mucho programar")
+
+saludo = "Hola Mundo"
+mi_nombre = "fulanito"
+edad = 20
+hobbie = "programar"
+
+print("\n\n") #Salto de linea
+
+print(saludo)
+print ("Bienvenido a NodeSchool")
+print ("Mi nombre es {}".format(mi_nombre))
+print ("Tengo {} años".format(edad))
+print ("y me gusta mucho {}".format(hobbie))
---
+++ variables.py
@@ -0,0 +1,24 @@
+nombre = "Fulanito Fulanovsky"
+usa_lentes = True
+tiene_mascota = False
+edad = 25
+pasatiempos = ["Correr", "Cocinar", "Leer", "Bailar"]
+celulares = {
+   "xiaomi redmi": {
+      "RAM": 3,
+      "ROM": 32,
+      "Procesador": 2.1
+   },
+      "iphone xs": {
+      "RAM": 4,
+      "ROM": 64,
+      "Procesador": 2.6
+   },
+}
+
+print("Nombre: {}".format(nombre))
+print("Usa lentes?: {}".format(usa_lentes))
+print("Tiene mascotas?: {}".format(usa_lentes))
+print("Edad: {}".format(edad))
+print("Pasatiempos: {}".format(pasatiempos))
+print("Celulares: {}".format(celulares))
