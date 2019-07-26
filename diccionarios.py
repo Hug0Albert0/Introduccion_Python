dab6a1a383886bbfa6ade82a9865fc39a5479620
hug0albert0 <hugorivera.cool.player@gmail.com>
2019-07-26 17:21:02 -0500

    if - for - diccionarios

---
+++ diccionarios.py
@@ -0,0 +1,9 @@
+producto = {
+   "clave": "AB003",
+   "nombre": "Aceite comestibe 500ml",
+   "marca": "Capullo",
+   "precio": 25.50
+}
+
+#for clave in producto:
+#  print (clave, ":", producto[clave])
---
+++ if_for.py
@@ -0,0 +1,13 @@
+salario_quincenal = 5000
+gastos_quincenales = 3500
+if salario_quincenal > gastos_quincenales:
+   print("Estable ")
+elif salario_quincenal == gastos_quincenales:
+   print("Apenas sobreviviendo")
+else:
+   print("Camino a la quiebra...")
+
+
+nombres = ["Hugo", "Paco", "Luis", "Donald", "Mickey"]
+for nombre in nombres:
+   print("Buenos días {} !!!".format(nombre))
